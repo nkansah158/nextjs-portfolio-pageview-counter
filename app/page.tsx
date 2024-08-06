4326e41ece11635626ed2cd8bf558affc3eb2f9a
nkansah158 <69765169+nkansah158@users.noreply.github.com>
2024-08-06 23:27:16 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
          <h2 className="text-sm text-zinc-500 ">
-          to my portfolio - Lord Nkansah
+           My portfolio | Lord Nkansah
         </h2>
         <h2 className="text-sm text-zinc-500 ">
           I'm building{" "}
